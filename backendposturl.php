345974320a8b364fa83ea9ae3fe755b540529527
vectoralbertlee <vectoralbertlee@outlook.com>
2017-05-16 18:59:33 +0800

    Create backendposturl.php

---
+++ backendposturl.php
@@ -0,0 +1,13 @@
+<?PHP
+$merchantcode = $_REQUEST["MerchantCode"];
+$paymentid = $_REQUEST["PaymentId"];
+$refno = $_REQUEST["RefNo"];
+$amount = $_REQUEST["Amount"];
+$ecurrency = $_REQUEST["Currency"];
+$remark = $_REQUEST["Remark"];
+$transid = $_REQUEST["TransId"];
+$authcode = $_REQUEST["AuthCode"];
+$status = $_REQUEST["Status"];
+$errdesc = $_REQUEST["ErrDesc"];
+$signature = $_REQUEST["Signature"];
+?>
